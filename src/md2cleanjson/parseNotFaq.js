63330b76653852dee4061d9e73da0320285acf6d
austinibele <austin.ibele@kungfu.ai>
2024-01-31 16:11:00 -0500

    Convert Markdown syntax to HTML for list items (forgot to do this earlier)

--- src/md2cleanjson/parseNotFaq.js
+++ src/md2cleanjson/parseNotFaq.js
@@ -37,31 +37,32 @@ function containsInvalidTag(str) {
     return invalidTagRegex.test(str);
   }
 
+const convertMdtoHtml = (line) => {
+    // Replace bold markdown with HTML <b> tags
+    line = line.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
+    line = line.replace(/__(.*?)__/g, '<b>$1</b>');
+
+    // Replace italic markdown with HTML <i> tags
+    // Updated regex pattern for italics, accounting for edge cases and ignoring underscores in words/identifiers
+    const italicRegex = /(?<!\w)(?<!\\)_([^\s_](?:.*?[^\s_])?)(?<!\\)_(?!\w)/g;
+    line = line.replace(italicRegex, function(match, content) {
+        // The "content" captured group contains the text to be italicized
+        return `<i>${content}</i>`;
+    });
+
+    // Replace markdown links with HTML <a> tags
+    line = line.replace(/\[([^\]]+)]\(([^)]+)\)/g, '<a href="$2">$1</a>');
+
+    return line;
+}
+
 const parseParagraph = (line) => {
     if (containsInvalidTag(line)) {
         return null;
     }
     line = line.trim();
     if (line.length > 0) {
-        let boldCount = 0;
-        let italicCount = 0;
-
-        // Replace bold markdown with HTML <b> tags
-        line = line.replace(/\*\*(.*?)\*\*/g, '<b>$1</b>');
-        line = line.replace(/__(.*?)__/g, '<b>$1</b>');
-
-        // Replace italic markdown with HTML <i> tags
-        // Updated regex pattern for italics, accounting for edge cases and ignoring underscores in words/identifiers
-        const italicRegex = /(?<!\w)(?<!\\)_([^\s_](?:.*?[^\s_])?)(?<!\\)_(?!\w)/g;
-        line = line.replace(italicRegex, function(match, content) {
-            // The "content" captured group contains the text to be italicized
-            italicCount++;
-            return `<i>${content}</i>`;
-        });
-
-        // Replace markdown links with HTML <a> tags
-        line = line.replace(/\[([^\]]+)]\(([^)]+)\)/g, '<a href="$2">$1</a>');
-
+        line = convertMdtoHtml(line);
         return {
             type: "paragraph",
             text: line
@@ -134,10 +135,7 @@ const parseNotFaq = (mdContent) => {
 
         const listItem = parseListItem(line);
         if (listItem) {
-            console.log(listItem);
-            console.log(line);
-            console.log('********')
-            listItems.push(listItem);
+            listItems.push(convertMdtoHtml(listItem));
             continue;
         }
 
